0fdb0ca7e14776185ed78593f9cba57aa2a62d5a
VallyProg <133013769+VallyProg@users.noreply.github.com>
2023-07-02 16:35:28 +0300

    Add files via upload

---
+++ work6.py
@@ -0,0 +1,16 @@
+# def f(a, b):
+#     if b == 0:
+#         return 1
+#     return a * f(a, b - 1)
+
+# a = int(input('Введите число: '))
+# b = int(input('Введите степень: '))
+# print(f(a, b))
+
+
+# def f(a, b):
+#     if b == 0:
+#         return a
+#     return 1 + f(a, b - 1)
+
+# print(f(2,2))
